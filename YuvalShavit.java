3b49c4be2553c016892982b1ea65918eacbddf4b
yuvalshavit1 <yuvalsha123@gmail.com>
2022-11-13 19:39:46 +0200

    This is test2

--- YuvalShavit.java
+++ YuvalShavit.java
@@ -1,7 +1,7 @@
 public class YuvalShavit {
   public static void main(String[] args){
        for(int i=1;i<=10;i++){
-	      System.out.println("Yuval Shavit test1..."+i);
+	      System.out.println("Yuval Shavit test2..."+i);
 	    }
 	 }
   }
